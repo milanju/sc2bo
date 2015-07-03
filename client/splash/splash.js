59e60acda89990c08d32f4cb0f2e312718c669c4
milanju <Milan.Jurhan@live.de>
2015-07-03 21:59:09 +0200

    Add buildorder list to splash screen

---
+++ client/splash/splash.js
@@ -0,0 +1,12 @@
+Template.splash.helpers({
+  'buildOrders': function() {
+    return BuildOrders.find({});
+  }
+});
+
+Template.splash.events({
+  'click .build-order-li': function(event) {
+    Session.set("activeBo", this._id);
+    Session.set("status", "editBo");
+  }
+})
